65347831391c0ca2a0bd4468e2c254426a86fbb9
KyeongSoo Yoo <dev.yoogomja@gmail.com>
2019-02-18 00:26:38 +0900

    make test data
    Signed-off-by: KyeongSoo Yoo <dev.yoogomja@gmail.com>

--- routes/API/boards.js
+++ routes/API/boards.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 import { Crawler } from '../../lib/crawler/crawler';
+import { prependOnceListener } from 'cluster';
 // var Crawler = require('../../lib/crawler/crawler');
 
 /* GET users listing. */
@@ -13,6 +14,27 @@ router.get('/', function(req, res, next) {
   .catch(function(){ 
     console.log(arguments); 
   });
+});
+
+router.get('/example' , function(req, res, next){
+  var __TEST_DATA = {};
+  __TEST_DATA.status = 'SUCCESS';
+  __TEST_DATA.data = [];
+  var mails = ['gmail.com' , 'naver.com' , 'hotmail.com' , 'hanmail.net' , 'syu.ac.kr'];
+  for(var i = 0 ; i < 20 ; i++){
+    var person = {};
+    person.seq = i;
+    person.name = 'person_' + i;
+    person.hp = '010-' + (Math.floor((Math.random() * 9000) + 1000)) + '-' + (Math.floor((Math.random() * 9000) + 1000));
+    person.mail = person.name + '@' + mails[Math.floor((Math.random()) * 5)];
+    person.grade = Math.floor(Math.random() * 4) + 1;
+    __TEST_DATA.data.push(person);
+  }
+  
+  res.set('Content-Type' , 'application/json');
+  res.send(
+    __TEST_DATA
+  );
 });
 
 module.exports = router;
