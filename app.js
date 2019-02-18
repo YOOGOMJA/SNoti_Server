2d0436a42b8a9de750f56d03ef84f7b1d1463992
KyeongSoo Yoo <dev.yoogomja@gmail.com>
2019-02-18 15:42:48 +0900

    cors
    Signed-off-by: KyeongSoo Yoo <dev.yoogomja@gmail.com>

--- app.js
+++ app.js
@@ -29,6 +29,12 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/api/boards', boardsRouter);
 
+app.all('/*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
