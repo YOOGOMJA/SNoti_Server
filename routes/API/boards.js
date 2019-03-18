730166df718624f875cb4c988e370ed02bfcc947
KyeongSoo Yoo <dev.yoogomja@gmail.com>
2019-03-18 15:22:10 +0900

    디비 오류 확인해야
    Signed-off-by: KyeongSoo Yoo <dev.yoogomja@gmail.com>

--- routes/API/boards.js
+++ routes/API/boards.js
@@ -5,6 +5,12 @@ import { Crawler } from '../../lib/crawler/crawler';
 import { prependOnceListener } from 'cluster';
 import { createDeflateRaw } from 'zlib';
 import { query } from '../../lib/db/query';
+import { FORMERR } from 'dns';
+
+let _CRAWLER_PAGE = 1;
+let _CRAWLER_WORKING = false;
+let _CRAWLER_LAST_PAGE = -1;
+let _CRAWLER_STOP_IMMEDIATELY = false;
 
 // var Crawler = require('../../lib/crawler/crawler');
 
@@ -70,19 +76,112 @@ router.get('/example' , function(req, res, next){
   );
 });
 
-router.get('/crawl' , function(req, res, next){
+router.get('/crawl/stop' , function(req,res,next){
+  res.set('Content-Type' , 'application/json');
+  if(_CRAWLER_WORKING){
+    _CRAWLER_STOP_IMMEDIATELY = true;
+
+    res.send({
+      status : 1,
+      mesg : '크롤링이 곧 중단됩니다'
+    });
+  }
+  else{
+    res.send({
+      status : 0,
+      mesg : '작동 중인 크롤링 프로세스가 없습니다'
+    });
+  }
+
+});
+
+router.get('/crawl/start' , function(req, res, next){
   res.set('Content-Type' , 'application/json');
 
-  query.request()
-  .then(function(data){
-    // console.log(data);
-    // res.send(data);
-    res.send({});
-  } , function(data){
-    console.log('??');
-    res.send({});
-  });
-  
+  // query.request()
+  // .then(function(data){
+  //   console.log(data);
+  //   // res.send(data);
+  //   console.log('Hello wOrld');
+  //   res.send({});
+  // } , function(data){
+  //   res.send(data);
+  // });
+
+
+  if(!_CRAWLER_WORKING){
+    res.send({ 
+      status : 1,
+      mesg : '크롤링 시작'
+    });
+    _CRAWLER_WORKING = true;
+    _CRAWLER_PAGE = 1;
+    
+    crawling();
+  }
+  else{
+    res.send({
+      status : 1,
+      mesg : '이미 크롤러가 작동중입니다. 현재 조회 페이지 : ' + _CRAWLER_PAGE,
+    });
+  }
 });
+
+/// crawler 
+function crawling(){
+  if(_CRAWLER_STOP_IMMEDIATELY){ 
+    console.log('크롤링이 중단되었습니다. 현재 페이지 : ' +_CRAWLER_PAGE);
+    _CRAWLER_WORKING = false;
+    _CRAWLER_STOP_IMMEDIATELY = false;
+    return; 
+  }
+  console.log('page : ' + _CRAWLER_PAGE + ' / ' + _CRAWLER_LAST_PAGE);
+
+  Crawler.get({
+    page : _CRAWLER_PAGE
+  })
+  .then(data => {
+    return new Promise((resolve , reject)=>{
+      _CRAWLER_LAST_PAGE = data.data.paging.last;
+      _CRAWLER_PAGE++;
+      if(_CRAWLER_PAGE <= _CRAWLER_LAST_PAGE){
+        // crawling();
+        resolve({
+          status : 1,
+          mesg : '진행중',
+          items : data.data.items
+        });
+      }
+      else{
+        _CRAWLER_WORKING = false;
+        console.log('finished!' , test.length);
+        resolve({
+          status : 0,
+          mesg : '종료'
+        });
+      }
+    });
+  })
+  .then(function(data){
+    if(data.status === 1){
+      // for(var i = 0 ; i < data.items.length ; i++){
+      //   console.log(data.items[i].title);
+      // }
+
+      query.insert(data.items[0]);
+
+    }
+    else if(data.status === 0){
+      console.log('종료!');
+    }
+    else{
+      console.log('오류 발생' , data);
+    }
+  });
+}
+
+function insert(item){
+
+}
 
 module.exports = router;
